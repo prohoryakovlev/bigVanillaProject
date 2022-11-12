cdb53d43e2403d486feeb631a4bb884c45af81aa
Prohor <prohor-yakovlev@mail.ru>
2022-11-12 17:53:10 +0300

    Test04

--- src/01-hello-test/01.test.ts
+++ src/01-hello-test/01.test.ts
@@ -1,26 +1,29 @@
 import {mult, splitInterWords, sum} from "./01";
 
+let a: number
+let b: number
+let c: number
+
+beforeEach(() => {
+    a = 1
+    b = 2
+    c = 3
+})
 
 test('sum should be correct', () => {
 
-    const a = 1
-    const b = 2
-    const c = 3
-
     const result1 = sum(a, b)
-    const result2 = sum(b, c)
+    a = 10
+    const result2 = sum(a, b)
 
     expect(result1).toBe(3)
-    expect(result2).toBe(5)
+    expect(result2).toBe(102)
+
+
 })
 
 
 test('multiply should be correct', () => {
-
-    const a = 1
-    const b = 2
-    const c = 3
-
     const result1 = mult(a, b)
     const result2 = mult(b, c)
 
